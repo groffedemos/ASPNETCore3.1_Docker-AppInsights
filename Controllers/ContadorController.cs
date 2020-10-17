4ebc4d8c9f22f24121e77de316f483b0432b30c5
Renato Groffe <renato.groffe@yahoo.com.br>
2020-10-17 14:58:18 -0300

    Update ContadorController.cs

--- Controllers/ContadorController.cs
+++ Controllers/ContadorController.cs
@@ -23,7 +23,7 @@ namespace APIContagem.Controllers
                 {
                     _CONTADOR.ValorAtual,
                     Environment.MachineName,
-                    Local = "Teste",
+                    Local = "Teste com deployment automatizado via GitHub Actions",
                     Sistema = Environment.OSVersion.VersionString,
                     Variavel = configuration["TesteAmbiente"],
                     TargetFramework = Assembly
